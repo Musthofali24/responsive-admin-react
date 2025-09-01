1b6a2d75e59399ddc13b713ba2ce3d633b9c47c7
Musthofali24 <almus2610@gmail.com>
2025-09-01 08:30:28 +0700

    feat(Navigation): Add Sidebar and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="font-quickSand">
+      <Header
+        toggleDarkMode={toggleDarkMode}
+        darkMode={darkMode}
+        toggleSidebar={toggleSidebar}
+      />
+
+      <Sidebar isSidebarOpen={isSidebarOpen} />
+    </div>
   );
 }
 
