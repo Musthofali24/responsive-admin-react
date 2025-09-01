da6e01af83bb84ae8e9de56e665fc2d175e3febd
Musthofali24 <almus2610@gmail.com>
2025-09-01 09:08:47 +0700

    feat(Content): Add Content Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Content from "./ui/Content";
+import Main from "./ui/Main";
+import Profile from "./Profile/Profile";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -28,6 +31,17 @@ function App() {
       />
 
       <Sidebar isSidebarOpen={isSidebarOpen} />
+
+      <Main>
+        <Content>
+          <div className="w-full grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="h-56 bg-white"></div>
+            <div className="h-56 bg-white"></div>
+            <div className="h-56 bg-white"></div>
+            <div className="h-56 bg-white"></div>
+          </div>
+        </Content>
+      </Main>
     </div>
   );
 }
